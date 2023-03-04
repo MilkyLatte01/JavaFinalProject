22bf1a5fabba87f4e850ef018136730cdfe50555
MilkyLatte <118425427+MilkyLatte01@users.noreply.github.com>
2023-03-04 22:34:27 +0700

    Project Progress 02

---
+++ ReallyJavaFinalProject/src/Testing.java
@@ -0,0 +1,24 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.management.InstanceNotFoundException;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+
+public class Testing{
+
+	public static void main(String[] args) {
+		
+	LogInPage LogInPage = new LogInPage();	
+	}
+
+	
+	
+}
