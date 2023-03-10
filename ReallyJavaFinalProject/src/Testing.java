f0aa67310921530dde4f0c7f9df21cf4cdaea315
MilkyLatte <118425427+MilkyLatte01@users.noreply.github.com>
2023-03-10 17:20:23 +0700

    Progress05(FrontEndComplete)

--- ReallyJavaFinalProject/src/Testing.java
+++ ReallyJavaFinalProject/src/Testing.java
@@ -1,16 +1,3 @@
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.management.InstanceNotFoundException;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-
-
 
 public class Testing{
 
@@ -18,8 +5,5 @@ public class Testing{
 		
 	LogInPage LogInPage = new LogInPage();	
 	
-	}
-
-	
-	
+	}	
 }
